e9856e38d50cf104c8ef6e70c876f816b596b272
lenovo <lenovo@DESKTOP-FLQ90G6>
2022-08-25 23:08:29 +0530

    Assignment

--- Basic/src/com/day9/copyOfRangeInArray.java
+++ Basic/src/com/day9/copyOfRangeInArray.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 public class copyOfRangeInArray {
 
 	public static void main(String[] args) {
-		
 		// copyOfRange is use to Copies the specified range of the specified array into a new array.
 		
-		int Array1[] = new int[] {5,10,15,6,7};   // Creating the Array.
-		System.out.println("Array1 is: " + Arrays.toString(Array1));	// Printing the Array using Arrays.toString().
-		
-		int Array2[] = Arrays.copyOfRange(Array1, 0, 3);   // Copy the element from Array1 to Array2 index from 0 to 3.
-		System.out.println("Array2 is: " + Arrays.toString(Array2));   // Printing the coped element from Array1 to Array2.
+				int Array1[] = new int[] {5,10,15,6,7};   // Creating the Array.
+				System.out.println("Array1 is: " + Arrays.toString(Array1));	// Printing the Array using Arrays.toString().
+				
+				int Array2[] = Arrays.copyOfRange(Array1, 0, 3);   // Copy the element from Array1 to Array2 index from 0 to 3.
+				System.out.println("Array2 is: " + Arrays.toString(Array2));   // Printing the coped element from Array1 to Array2.
 
 	}
 
