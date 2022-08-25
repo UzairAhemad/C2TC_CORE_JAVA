07a0cc05af051050d3711561510c74d9d86522f5
lenovo <lenovo@DESKTOP-FLQ90G6>
2022-08-25 21:20:45 +0530

    Assignment

---
+++ Basic/src/com/day9/ArrayListAndAsList.java
@@ -0,0 +1,31 @@
+package com.day9;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+ 
+public class ArrayListAndAsList {
+
+	public static void main(String[] args) {
+		
+		List<String> citysname = new ArrayList<>();      // Creating Array List.
+		citysname.add("Mumbai");	// This is element.
+		citysname.add("Pune");
+		citysname.add("Nashik");
+		citysname.add("Nagpur");
+		citysname.add("Thane");
+		
+		String[] city = {"Mumbai","Pune","Nashik","Nagpur","Thane"};  // Creating String Array.
+		List<String> list = Arrays.asList(city); // "asList" is returning an Immutable ArrayList (Can't add any element).
+		
+		citysname.add("New Mumbai");  // Adding New Element Here in cityname.
+		System.out.println(citysname); 
+		
+		list.add("New Mumbai");  // Adding New Element Here in city.
+		System.out.println(list);
+		
+		
+
+	}
+
+}
