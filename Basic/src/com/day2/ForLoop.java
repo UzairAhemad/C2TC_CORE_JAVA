b479cb328d0beb9b31f245b7a58eace9216810f8
lenovo <lenovo@DESKTOP-FLQ90G6>
2022-09-04 15:51:28 +0530

    First Commit

---
+++ Basic/src/com/day2/ForLoop.java
@@ -0,0 +1,19 @@
+package com.day2;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		/*
+		 FOR LOOP:-
+		 The Java for loop is used to iterate a part of the program several times. 
+		 If the number of iteration is fixed, it is recommended to use for loop.
+		 */
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+			
+	}
+
+}
